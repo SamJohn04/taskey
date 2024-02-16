156af4508c739cb9aba2f53f2a11d2748d2a2fe2
SamJohn04 <samuel.john.codes@gmail.com>
2024-02-16 20:48:56 +0530

    route refactor

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -3,7 +3,6 @@ import TaskList from "../components/TasksList"
 import { getTasks } from "../mongo/Controller/taskController"
 import { redirect } from "next/navigation";
 import TaskNav from "./components/TaskNav";
-import Button from "../components/styleComponents/Button";
 
 export default async function Tasks() {
     const { user } = await getSession() ?? {};
@@ -17,10 +16,9 @@ export default async function Tasks() {
     }
     return (
         <main className="w-[95dvw] mt-20 mx-auto flex flex-col gap-4 items-center justify-center">
-            <div className="w-full flex justify-between items-center"><Button className="rounded hover:scale-95 justify-self-end">New Task</Button></div>
             <div className="w-full flex gap-4">
                 <TaskNav />
-                <div className="w-[95vw] max-w-6xl"><TaskList tasks={result.tasks}/></div>
+                <div className="w-[95dvw] max-w-6xl"><TaskList tasks={result.tasks}/></div>
             </div>
         </main>
     )
